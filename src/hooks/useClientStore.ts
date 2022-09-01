f668a5fb641af6226e6e9c01b6f1667168ec73aa
guillermo <guillermoambroggio@gmail.com>
2022-09-01 20:33:25 -0300

    :bug: Change types in clientStore

--- src/hooks/useClientStore.ts
+++ src/hooks/useClientStore.ts
@@ -8,17 +8,21 @@ import {
 } from '../particules/clientStore/clientStore.types';
 import reducer from '../particules/clientStore/reducers';
 
-const immer =
+/* const immer =
   <T extends State>(
     config: StateCreator<T, (fn: (draft: T) => void) => void>,
   ): StateCreator<T> =>
   (set, get, api) =>
-    config((fn) => set(produce(fn) as (state: T) => T), get, api);
+    config((fn) => set(produce(fn) as (state: T) => T), get, api); */
+
+const immer = (config: any) => (set: any, get: any, api: any) =>
+  config((fn: any) => set(produce(fn)), get, api);
 
 const useClientStore = create<ClientStore & ClientDispatch>(
-  immer((set) => ({
+  immer((set: (arg0: (state: ClientStore) => void) => any) => ({
     ...initialClientStore,
-    dispatch: (args: Actions) => set((state) => reducer(state, args)),
+    dispatch: (args: Actions) =>
+      set((state: ClientStore) => reducer(state, args)),
   })),
 );
 
